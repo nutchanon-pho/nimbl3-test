4ea6eccf7838f49558f87b28e33af21a19214a73
Nutchanon Phongoen <nutchanon.pho@gmail.com>
2017-09-06 23:18:32 +0700

    Add es6 for jshint options

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,10 @@ module.exports = function (grunt) {
         // JS TASKS ================================================================
         // check all js files for errors
         jshint: {
-            all: ['public/js/*.js']
+            all: ['public/js/*.js'],
+            options: {
+                'esversion': 6,
+            }
         },
 
         // take all the js files and minify them into app.min.js
