b90fb09d89c81a8a8f4d21951906c646d2e6e0e8
Nutchanon Phongoen <nutchanon.pho@gmail.com>
2017-09-07 22:09:36 +0700

    Sidenav with color bar and dynamic menu from controller

--- public/js/MainController.js
+++ public/js/MainController.js
@@ -1,6 +1,107 @@
 var app = angular.module('nimbl3', ['ngCookies']);
 app.controller('MainController', ['$scope', '$http', '$cookies', '$httpParamSerializer', function ($scope, $http, $cookies, $httpParamSerializer) {
-    
+    console.log(angular.element('.active'));
+
+    $scope.currentActiveMenuIndex = 0;
+
+    $scope.sideNavMenuList = [
+        {
+            label: 'Dashboard',
+            image: {
+                active: '/icons/controls-white.svg',
+                inactive: '/icons/controls-purple.svg'
+            },
+            isActive: true,
+            colorBar: '#FFCC00'
+        },
+        {
+            label: 'Orders',
+            image: {
+                active: '/icons/shopping-cart-white.svg',
+                inactive: '/icons/shopping-cart-purple.svg'
+            },
+            isActive: false,
+            colorBar: '#65B32E'
+        },{
+            label: 'Companies',
+            image: {
+                active: '/icons/factory-white.svg',
+                inactive: '/icons/factory-purple.svg'
+            },
+            isActive: false,
+            colorBar: '#65B32E'
+        },
+        {
+            label: 'Products',
+            image: {
+                active: '/icons/box-white.svg',
+                inactive: '/icons/box-purple.svg'
+            },
+            isActive: false,
+            colorBar: '#65B32E'
+        },
+        {
+            label: 'Documents',
+            image: {
+                active: '/icons/document-white.svg',
+                inactive: '/icons/document-purple.svg'
+            },
+            isActive: false,
+            colorBar: '#65B32E'
+        },
+        {
+            label: 'Pricing',
+            image: {
+                active: '/icons/pricing-label-white.svg',
+                inactive: '/icons/pricing-label-purple.svg'
+            },
+            isActive: false,
+            colorBar: '#65B32E'
+        },
+        {
+            label: 'Brands',
+            image: {
+                active: '/icons/shapes-white.svg',
+                inactive: '/icons/shapes-purple.svg'
+            },
+            isActive: false,
+            colorBar: '#65B32E'
+        },
+        {
+            label: 'Settings',
+            image: {
+                active: '/icons/gear-white.svg',
+                inactive: '/icons/gear-purple.svg'
+            },
+            isActive: false,
+            colorBar: '#65B32E'
+        },
+        {
+            label: 'Reports',
+            image: {
+                active: '/icons/chart-white.svg',
+                inactive: '/icons/chart-purple.svg'
+            },
+            isActive: false,
+            colorBar: '#65B32E'
+        },
+        {
+            label: 'Account Users',
+            image: {
+                active: '/icons/people-white.svg',
+                inactive: '/icons/people-purple.svg'
+            },
+            isActive: false,
+            colorBar: '#65B32E'
+        }
+    ];
+
+    $scope.setMenuAsActive = (index) => {
+        $scope.sideNavMenuList[$scope.currentActiveMenuIndex].isActive = false;
+        $scope.sideNavMenuList[index].isActive = true;
+        $scope.currentActiveMenuIndex = index;
+    };
+
     $scope.login = () => {
         let data = {
             grant_type:'password', 
