4bba843303438fb8fd51ad642710ce13954c6e46
Nutchanon Phongoen <nutchanon.pho@gmail.com>
2017-09-07 10:39:27 +0700

    Mock get product list from server

--- app.js
+++ app.js
@@ -32,18 +32,60 @@ app.oauth = new oauthServer({
 app.post('/oauth/token', app.oauth.token());
 
 // Get secret.
-app.get('/secret', app.oauth.authenticate(), function(req, res) {
+app.get('/secret', app.oauth.authenticate(), function (req, res) {
   // Will require a valid access_token.
   res.send('Secret area');
 });
 
-app.get('/public', function(req, res) {
+app.get('/public', function (req, res) {
   // Does not require an access_token.
   res.send('Public area');
 });
 
-app.get('/', function(req, res, next) {
+app.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// Get products
+app.get('/products', app.oauth.authenticate(), function (req, res) {
+  let productList = [
+    {
+      id: 1,
+      sku: 'TN SO 000015',
+      description: 'Magnetic contactor - 9A (5.5 kW, 7.5 HP) control voltage 230 Vac',
+      brand: 'Schneider Electric',
+      listPrice: 880
+    },
+    {
+      id: 2,
+      sku: 'TN SO 000015',
+      description: 'Magnetic contactor - 9A (5.5 kW, 7.5 HP) control voltage 230 Vac',
+      brand: 'Schneider Electric',
+      listPrice: 880
+    },
+    {
+      id: 3,
+      sku: 'TN SO 000015',
+      description: 'Magnetic contactor - 9A (5.5 kW, 7.5 HP) control voltage 230 Vac',
+      brand: 'Schneider Electric',
+      listPrice: 880
+    },
+    {
+      id: 4,
+      sku: 'TN SO 000015',
+      description: 'Magnetic contactor - 9A (5.5 kW, 7.5 HP) control voltage 230 Vac',
+      brand: 'Schneider Electric',
+      listPrice: 880
+    },
+    {
+      id: 5,
+      sku: 'TN SO 000015',
+      description: 'Magnetic contactor - 9A (5.5 kW, 7.5 HP) control voltage 230 Vac',
+      brand: 'Schneider Electric',
+      listPrice: 880
+    }
+  ];
+  return res.json(productList);
 });
 
 module.exports = app;
